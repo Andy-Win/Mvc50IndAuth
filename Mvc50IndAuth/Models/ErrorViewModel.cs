b817a5ea6104b149920a10c926b888bd5df57664
Andy Wang <v-wajian@microsoft.com>
2021-05-07 02:00:37 -0700

    Add project files.

---
+++ Mvc50IndAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mvc50IndAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
